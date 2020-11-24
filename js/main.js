adfef08b09e5dea00167d0f6b3012b5e3d52c2f8
Glenn Tippett <glenn.tippett@gmail.com>
2020-11-25 07:42:38 +1100

    add some comments to js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+// Open full-screen nav on Burger click
 document.querySelector('.open-nav').addEventListener('click', () => {
   document.querySelector('.nav-container').style.display = 'grid';
 });
 
+// Close Nav on X click
 document.querySelector('.close-nav').addEventListener('click', () => {
   document.querySelector('.nav-container').style.display = 'none';
 });
