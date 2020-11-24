29173f58016e7da38808fc4f3cf84f33257b3c4a
Glenn Tippett <glenn.tippett@gmail.com>
2020-11-25 08:35:38 +1100

    adjust card responsiveness

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ document.querySelector('.open-nav').addEventListener('click', () => {
 document.querySelector('.close-nav').addEventListener('click', () => {
   document.querySelector('.nav-container').style.display = 'none';
 });
+
+if (screen.width <= 768) {
+  cardTwoImg = document.querySelector('#card-img-2');
+  cardTwoImgParent = document.querySelector('#card-two-parent');
+  cardTwoImgParent.insertBefore(cardTwoImg, cardTwoImgParent.childNodes[0]);
+}
